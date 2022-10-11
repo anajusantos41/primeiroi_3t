fbda3ce92652e677d9bbaab8cbd98c5bc775d88a
ana julia dos santos <110939397+anajusantos41@users.noreply.github.com>
2022-10-11 17:50:50 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -39,3 +39,30 @@ function total(){
     let resultado = (val * ((ju/100)+1));
     document.write("O total é de: " + resultado);
 }
+// Para dividir usa a / 
+// Calcular a média das 4 notas. Somas as 4 notas e divide por 4.
+// Quanto falta para passar. Pega 240 e subtrai as notas. 
+function soma(){
+     let n1 = document.getElementById("b1").value; 
+     let n2 = document.getElementById("b2").value; 
+     let n3 = document.getElementById("b3").value; 
+     let n4 = document.getElementById("b4").value; 
+     let r = Number(n1) + Number(n2) + Number(n3) + Number(n4); 
+     document.getElementById("resultado").innerHTML = r; 
+    }
+    function soma(){
+        let n1 = document.getElementById("b1").value; 
+        let n2 = document.getElementById("b2").value; 
+        let n3 = document.getElementById("b3").value; 
+        let n4 = document.getElementById("b4").value; 
+        let r = Number(n1) + Number(n2) + Number(n3) + Number(n4); 
+        document.getElementById("resultado").innerHTML = r; 
+}
+        function média(){
+        let n1 = document.getElementById("b1").value; 
+        let n2 = document.getElementById("b2").value; 
+        let n3 = document.getElementById("b3").value; 
+        let n4 = document.getElementById("b4").value; 
+        let r = 240 -Number(n1) + Number(n2) + Number(n3) + Number(n4); 
+        document.getElementById("resultado").innerHTML = r; 
+    }
