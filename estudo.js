154c6171adbae5b0a50f949a851ba6a296f15653
ana julia dos santos <110939397+anajusantos41@users.noreply.github.com>
2022-10-25 17:50:00 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -33,17 +33,19 @@ function quadrado(){
     document.write("O quadrado de " + i + " é " + (i*i)+"<br>")
     }
 }
-
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
-
+    
     let t = document.getElementById("meses").value;
     let resultado = 0;
-    for(let m = 1; m < t; m++){
+    for(let m = 1; m <= t; m++){
         resultado = (val * ((ju/100)+1));
         val = resultado;
-        document.write("Mẽs " + m + " valor de " + val + "<br>");
+        document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
     }
-    document.write("O total é de: " + resultado);
+    document.write("O total é de: " + moeda(resultado) );
 }
